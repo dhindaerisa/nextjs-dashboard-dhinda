3b3c298d9219d466f62ec6d87e5e636d863400c8
Dhinda Erisa <dhindaerisa88@gmail.com>
2026-05-17 09:54:16 +0700

    add search and pagination

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -8,14 +8,14 @@ import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 
 import { fetchInvoicesPages } from '@/app/lib/data';
 
-export default async function Page({
-  searchParams,
-}: {
-  searchParams?: {
+export default async function Page(props: {
+  searchParams?: Promise<{
     query?: string;
     page?: string;
-  };
+  }>;
 }) {
+  const searchParams = await props.searchParams;
+
   const query = searchParams?.query || '';
   const currentPage = Number(searchParams?.page) || 1;
 
