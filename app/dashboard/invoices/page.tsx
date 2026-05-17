b2f1d4ecce4b5616706dff20c43353ed97d9c48f
Dhinda Erisa <dhindaerisa88@gmail.com>
2026-05-17 10:50:44 +0700

    fix vercel build

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -5,19 +5,20 @@ import { CreateInvoice } from '@/app/ui/invoices/buttons';
 import { lusitana } from '@/app/ui/fonts';
 import { Suspense } from 'react';
 import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
-
 import { fetchInvoicesPages } from '@/app/lib/data';
 
-export default async function Page(props: {
-  searchParams?: Promise<{
+export default async function Page({
+  searchParams,
+}: {
+  searchParams: Promise<{
     query?: string;
     page?: string;
   }>;
 }) {
-  const searchParams = await props.searchParams;
+  const params = await searchParams;
 
-  const query = searchParams?.query || '';
-  const currentPage = Number(searchParams?.page) || 1;
+  const query = params?.query || '';
+  const currentPage = Number(params?.page) || 1;
 
   const totalPages = await fetchInvoicesPages(query);
 
